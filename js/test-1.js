260c356a2b649e5ac14e5bbedaa5c8f7b4ef12f4
Oleksii_S <lex.sydoruk@gmail.com>
2023-12-02 00:58:04 +0100

    ...

--- js/test-1.js
+++ js/test-1.js
@@ -1,1 +1,32 @@
-"use strict";
+https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/javascript
+
+// function reverseWords(str) {
+//   //   let result = [];
+//   //   let wordReversed;
+//   //   let strArray = str.split(' ');
+//   //   console.log(strArray);
+//   //   for (const word of strArray) {
+//   //     wordReversed = word.split('').reverse().join('');
+//   //     result.push(wordReversed);
+//   //   }
+//   //   return result.join(' ');
+//   return str
+//     .split(' ')
+//     .map(word => word.split('').reverse().join(''))
+//     .join(' ');
+// }
+
+////////////////////////////////////////////////////////////////////////////////
+
+https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/javascript
+
+// function reverseWords(str) {
+//   let str1 = str.split('').reverse().join('');
+//   console.log(str1);
+//   console.log(str1.split(' ').reverse().join(' '));
+//   return str.split('').reverse().join('').split(' ').reverse().join(' ');
+// }
+
+// reverseWords('hello world');
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
