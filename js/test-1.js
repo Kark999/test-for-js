d6de25dfb3fdfe0d0425f849e6bfb1219fd30f93
Andrey Svyrskyi <akkad1v@gmail.com>
2023-12-03 05:30:12 +0200

    Update test-1.js

--- js/test-1.js
+++ js/test-1.js
@@ -80,3 +80,40 @@
 
 // console.log(count('abbaaaccc'));
 // console.log(count('AABBBACCCACACC'));
+
+//===========================================================
+
+// function changeEven(numbers, value) {
+//   let result = [];
+
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 === 0) {
+//       result.push(numbers[i] + value);
+//     } else {
+//       result.push(numbers[i]);
+//     }
+//   }
+
+//   return result;
+// }
+// changeEven([2, 8, 3, 7, 4, 6], 10);
+// console.log(result);
+
+//===============================================================
+
+// const players = {
+//   mango: 1270,
+//   poly: 468,
+//   ajax: 710,
+//   kiwi: 244,
+// };
+// const playtimes = Object.values(players); // [1270, 468, 710, 244]
+
+// const totalPlayTime = playtimes.reduce((total, time) => total + time, 0);
+
+// const averagePlayTime = totalPlayTime / playtimes.length;
+
+// console.log(totalPlayTime);
+// console.log(averagePlayTime);
+
+//=================================================================================
