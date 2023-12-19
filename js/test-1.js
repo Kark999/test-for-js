93ac94189370cf24c61f4243f55e1ae5ae300f85
Andrey Svyrskyi <akkad1v@gmail.com>
2023-12-19 07:34:37 +0200

    Update test-1.js

--- js/test-1.js
+++ js/test-1.js
@@ -245,7 +245,73 @@
 //     [7, 10],
 //   ]),
 // );
-function addBinary(a, b) {
-  return typeof (a + b).toString(2);
-}
-console.log(addBinary(5, 7));
+// function addBinary(a, b) {
+//   return typeof (a + b).toString(2);
+// }
+// console.log(addBinary(5, 7));
+//================================================================
+// function solution(str, ending) {
+//   return str.endsWith(ending);
+// }
+// console.log(solution('abcde', 'cde'));
+//====================================================================
+
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))(("
+
+// function duplicateEncode(word) {
+//   const lowerCaseWord = word.toLowerCase();
+
+//   let result = '';
+
+//   for (let i = 0; i < lowerCaseWord.length; i++) {
+//     const count = lowerCaseWord.split(lowerCaseWord[i]).length - 1;
+
+//     result += count === 1 ? '(' : ')';
+//   }
+
+//   return result;
+// }
+// function duplicateEncode(word) {
+//   return word
+//     .toLowerCase()
+//     .split('')
+//     .map(function (a, i, w) {
+//       return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')';
+//     })
+//     .join('');
+// }
+
+// console.log(duplicateEncode('din'));
+// console.log(duplicateEncode('barRakuda'));
+//=========================================================
+
+// function quicksum(packet) {
+//   let quicksumValue = 0;
+
+//   for (let i = 0; i < packet.length; i++) {
+//     const currentChar = packet[i];
+
+//     if (/^[A-Z\s]$/.test(currentChar)) {
+//       if (currentChar !== ' ') {
+//         const charValue = currentChar.charCodeAt(0) - 'A'.charCodeAt(0) + 1;
+//         quicksumValue += charValue * (i + 1); // Додаємо індекс до суми
+//       }
+//     } else {
+//       quicksumValue = 0;
+//       break;
+//     }
+//   }
+
+//   return quicksumValue;
+// }
+
+// console.log(quicksum('MID CENTRAL')); //650
+// console.log(quicksum('234 234 WEF ASDF AAA 554211 ???? '));
+// console.log(quicksum('axg '));
+//=================================================================
