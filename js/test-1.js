193acf928c15631a53db821ee68e32147e61f03f
Andrey Svyrskyi <akkad1v@gmail.com>
2023-12-22 04:21:29 +0200

    Update test-1.js

--- js/test-1.js
+++ js/test-1.js
@@ -315,3 +315,17 @@
 // console.log(quicksum('234 234 WEF ASDF AAA 554211 ???? '));
 // console.log(quicksum('axg '));
 //=================================================================
+// function validateUsr(username) {
+//   const res = /^[a-z0-9_]{4,16}$/.test(username);
+//   return res;
+// }
+/**
+    - `^`        Start from the beginning of the string.
+    - `[]`       Allow any character specified, including...
+    - `a-z`      anything from a to z,
+    - `0-9`      anything from 0 to 9,
+    - `_`        and underscore.
+    - `{4,16}`   Accept 4 to 16 of allowed characters, both numbers included.
+    - `$`        End the string right after specified amount of allowed characters is given.
+  */
+//================================================================
